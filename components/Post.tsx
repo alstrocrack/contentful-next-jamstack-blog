7b8808ce51e5a9622119aa4ad12fc861dbceb61a
YutoUrushima <yuto259233@gmail.com>
2023-05-02 20:23:12 +0900

    Fix types

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,16 +1,18 @@
-function Post({ date, image, title }) {
-    let { file, description } = image
+import React from "react";
+import { contentImage } from "../models/contentfulContent";
 
-    return (
-        <div className="post">
-        <img alt={description} src={`https:${file.url}`} />
-        <div className="description">{description}</div>
-        <div className="text">
-            <h2>{title}</h2>
-            <h3>{date.substring(0, 10)}</h3>
-        </div>
-        </div>
-    )
-}
+const Post: React.FC<{ date: string; image: contentImage; title: string }> = ({ date, image, title }) => {
+  const { url, description } = image.fields.file;
+  return (
+    <div className="post">
+      <img alt={description} src={`https:${url}`} />
+      <div className="description">{description}</div>
+      <div className="text">
+        <h2>{title}</h2>
+        <h3>{date.substring(0, 10)}</h3>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
--- utils/contentfulPosts.ts
+++ utils/contentfulPosts.ts
@@ -1,18 +1,23 @@
-import contentful from "contentful";
+import * as contentful from "contentful";
+import { contentfulPost } from "../models/contentfulContent";
 
 const spaceID = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
-const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
+const accessToken = process.env.NEXT_PUBLIC_CONTENT_DELIVERY_API_KEY;
 
-const client = contentful.createClient({
-  space: "a",
-  accessToken: "a",
-});
-
-export async function fetchEntries() {
-  const entries = await client.getEntries();
-  if (entries.items) return entries.items;
+if (!spaceID || !accessToken) {
+  throw new Error("Environment variables not set");
 }
 
-export default {
-  fetchEntries,
+const client = contentful.createClient({
+  space: spaceID,
+  accessToken: accessToken,
+});
+
+const fetchEntries = async () => {
+  const entries = await client.getEntries();
+  let contents: contentfulPost[] | [] = [];
+  if (entries.items) contents = [...entries.items];
+  return contents;
 };
+
+export default fetchEntries;
