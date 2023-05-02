0646e328508cb89030fb487b81cee0d0098c29d0
YutoUrushima <yuto259233@gmail.com>
2023-05-02 23:34:48 +0900

    Use sass & Change style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import React from "react";
+import Header from "../components/Header";
+import styles from "../styles/Home.module.scss";
 import fetchEntries from "../utils/contentfulPosts";
 import Post from "../components/Post";
-import React from "react";
 import { contentfulData } from "../models/contentfulContent";
 
 interface contentfulProps {
@@ -12,34 +11,18 @@ interface contentfulProps {
 
 const Home: React.FC<contentfulProps> = ({ posts }) => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Contentful Next Jamstack Blog</title>
-
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="YutoUrushima" />
-        <meta property="og:description" content="Next.js + contentful" />
-        {/* <meta property="og:url" content="https://pedantic-hugle-406857.netlify.app" /> */}
-        <meta property="og:site_name" content="YutoUrushima" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="YutoUrushima" />
-        <meta name="twitter:title" content="YutoUrushima" />
-        <meta name="twitter:description" content="Next.js + contentful" />
-        {/* <meta name="twitter:image" content="https://pedantic-hugle-406857.netlify.app/b-ogp-image.jpg" /> */}
-        {/* <meta name="twitter:url" content="https://pedantic-hugle-406857.netlify.app" /> */}
-
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <div className="posts">
-          {posts.map((p) => {
-            return <Post key={p.date} date={p.date} image={p.image} title={p.title} />;
-          })}
-        </div>
-      </main>
-    </div>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <div className="posts">
+            {posts.map((p) => {
+              return <Post key={p.date} date={p.date} image={p.image} title={p.title} />;
+            })}
+          </div>
+        </main>
+      </div>
+    </>
   );
 };
 
