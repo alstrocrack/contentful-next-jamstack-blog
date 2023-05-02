5d5e1172c252efb1edce5a5da78ce87faabf2c4c
YutoUrushima <yuto259233@gmail.com>
2023-05-02 19:32:53 +0900

    Intro prettier

--- utils/contentfulPosts.ts
+++ utils/contentfulPosts.ts
@@ -1,20 +1,18 @@
-import contentful from 'contentful'
+import contentful from "contentful";
 
-const spaceID = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
-const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
-
-if()
-throw new Error
+const spaceID = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
+const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
 
 const client = contentful.createClient({
-  space: ,
-  accessToken: ,
-})
+  space: "a",
+  accessToken: "a",
+});
 
 export async function fetchEntries() {
-  const entries = await client.getEntries()
-  if (entries.items) return entries.items
-  console.log(`Error getting Entries for ${contentType.name}.`)
+  const entries = await client.getEntries();
+  if (entries.items) return entries.items;
 }
 
-export default { fetchEntries }
+export default {
+  fetchEntries,
+};
